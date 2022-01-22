461c607433d99ad6033921c95b2f8c017cb2aa02
Seb <sebcyde@hotmail.com>
2022-01-22 23:55:49 +0000

    added in guesses scorekepper

--- RandomNumGen.py
+++ RandomNumGen.py
@@ -14,5 +14,26 @@ else:
     quit()
 
 r = random.randint(0, top_of_range)
+guesses = 0
+
 print(r)
 
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+    else: 
+        print("Please type a number next time...")   
+        continue
+
+    if user_guess == r:
+        print("You got it!")
+        break
+      
+    else: 
+        print("You got it wrong!")
+        continue
+
+print("You got it in", guesses, "guesses")
