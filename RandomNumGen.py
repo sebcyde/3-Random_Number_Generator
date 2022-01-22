7339ffc9b008b9508dfd552c625e16e2697e40cf
Seb <sebcyde@hotmail.com>
2022-01-22 23:44:41 +0000

    added in code to check if it is a number and then convert it to an integer

--- RandomNumGen.py
+++ RandomNumGen.py
@@ -1,4 +1,18 @@
 import random 
 
-r = random.randrange(11)
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+
+else:
+    print("Please type a number next time.")
+    quit()
+
+r = random.randint(0, top_of_range)
 print(r)
+
